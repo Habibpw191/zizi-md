c644a57b1dd0132db6ac0ecf3afd042b81d6b73b
Habibpw191 <116631223+Habibpw191@users.noreply.github.com>
2022-10-25 18:06:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -31,11 +31,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker By'
-global.author = 'Naze Dev'
+global.owner = ['6289528935082']
+global.ownernomer = "6289528935082"
+global.premium = ['6289528935082']
+global.packname = 'Zii By'
+global.author = 'Zii'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
